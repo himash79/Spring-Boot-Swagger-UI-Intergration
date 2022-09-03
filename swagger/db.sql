a727dc67ef4e658a7bdd685b88b84f0e26db8a02
Yomal Sri Bandara <31383680+himash79@users.noreply.github.com>
2022-09-03 14:09:50 +0530

    Add files via upload
    Add DB file

---
+++ swagger/db.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `accessories_db` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `accessories_db`;
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: accessories_db
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `mobiles`
+--
+
+DROP TABLE IF EXISTS `mobiles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mobiles` (
+  `Mobile_Id` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `Mobile_Brand` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `Mobile_Model` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `Mobile_Cost` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `Mobile_Status` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`Mobile_Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mobiles`
+--
+
+LOCK TABLES `mobiles` WRITE;
+/*!40000 ALTER TABLE `mobiles` DISABLE KEYS */;
+INSERT INTO `mobiles` VALUES ('M0001','Motorola','Z101','$500','Active');
+/*!40000 ALTER TABLE `mobiles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-03 14:08:29
